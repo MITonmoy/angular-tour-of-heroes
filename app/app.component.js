d2db2a465516e96c43f2d10cd3f88ef50255183d
Mufle Ibnoon <mibnoon@bd.imshealth.com>
2016-11-11 19:48:48 +0600

    Up to tour of heroes - https://angular.io/docs/ts/latest/tutorial/toh-pt1.html section ~Adding more HTML~

--- app/app.component.js
+++ app/app.component.js
@@ -9,13 +9,27 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+/**
+ * Hero
+ 
+ */
+var Hero = (function () {
+    function Hero(parameters) {
+    }
+    return Hero;
+}());
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Tour of Heroes';
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>Hello Angular!</h1>'
+            templateUrl: '/app/app.component.html'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,25 @@
 import { Component } from '@angular/core';
+
+/**
+ * Hero
+ 
+ */
+class Hero {
+  id: number;
+  name: string;
+  constructor(parameters) { 
+  }
+}
+
 @Component({
   selector: 'my-app',
-  templateUrl: 'app.component.html'
+  templateUrl: '/app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero:Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  
+ }
